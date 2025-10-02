56b3f141d9e6b74f518aef3276c3b88f5331c116
Uzochukwu Joseph <josephkingsleyy@gmail.com>
2025-10-02 13:30:17 +0100

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './common/guards/roles.guard';
@@ -11,13 +10,14 @@ import { QuestionModule } from './question/question.module';
 import { ResponseModule } from './response/response.module';
 import { BillingModule } from './billing/billing.module';
 import { PaymentModule } from './payment/payment.module';
-import { Scheduler } from 'rxjs';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CronsModule } from './crons/crons.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Global()
 @Module({
-  imports: [PrismaModule,
+  imports: [
+    PrismaModule,
     AuthModule,
     TicketModule,
     SurveyModule,
@@ -26,13 +26,15 @@ import { CronsModule } from './crons/crons.module';
     BillingModule,
     PaymentModule,
     ScheduleModule.forRoot(),
-    CronsModule],
+    CronsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService,
+  providers: [
+    AppService,
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
-    }
+    },
   ],
 })
 export class AppModule { }
