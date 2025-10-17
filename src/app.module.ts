99dc96dbfa7d0da54556cee2316b598a780ec62b
Opemipo Alomaja <alomajaopemipo8@gmail.com>
2025-10-17 15:00:06 +0100

    payment and noti out

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,6 @@ import { AuthModule } from './auth/auth.module';
 import { BillingModule } from './billing/billing.module';
 import { TicketModule } from './ticket/ticket.module';
 import { CronsModule } from './crons/crons.module';
-import { NotificationModule } from './notification/notification.module';
-import { PaymentModule } from './payment/payment.module';
 
 
 @Module({
@@ -19,8 +17,6 @@ import { PaymentModule } from './payment/payment.module';
     AuthModule,
     BillingModule,
     CronsModule,
-    NotificationModule,
-    PaymentModule,
     TicketModule,
     ScheduleModule.forRoot(),
 
