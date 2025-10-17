1f4026a818515dbb9b1c666178c74f2fcb6518ed
Opemipo Alomaja <alomajaopemipo8@gmail.com>
2025-10-17 14:52:12 +0100

    fix: noti, payment

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { AuthModule } from './auth/auth.module';
 import { BillingModule } from './billing/billing.module';
 import { TicketModule } from './ticket/ticket.module';
 import { CronsModule } from './crons/crons.module';
+import { NotificationModule } from './notification/notification.module';
+import { PaymentModule } from './payment/payment.module';
 
 
 @Module({
@@ -17,6 +19,8 @@ import { CronsModule } from './crons/crons.module';
     AuthModule,
     BillingModule,
     CronsModule,
+    NotificationModule,
+    PaymentModule,
     TicketModule,
     ScheduleModule.forRoot(),
 
---
+++ src/payment/payment.controller.ts
@@ -0,0 +1,67 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { PaginationDto } from 'src/ticket/dto/update-ticket.dto';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+
+@Controller('payment')
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) { }
+
+  @Post()
+  create(@Body() createPaymentDto: CreatePaymentDto,
+    @CurrentUser('sub') sub: number,
+  ) {
+    return this.paymentService.create(createPaymentDto, sub);
+  }
+
+  @Roles('admin')
+  @Get()
+  findAll(
+    @Query() pagination: PaginationDto
+  ) {
+    return this.paymentService.findAll(pagination.page,
+      pagination.limit);
+  }
+
+  @Get('my-payments')
+  async findMyPayments(
+    @CurrentUser('sub') sub: number,
+    @Query() pagination: PaginationDto,
+  ) {
+    return this.paymentService.findMyPayments(sub,
+      pagination.page,
+      pagination.limit
+    );
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.paymentService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
+    return this.paymentService.update(+id, updatePaymentDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.paymentService.remove(+id);
+  }
+
+  @Post('initialize')
+  async initializePayment(@Body() dto: CreatePaymentDto,
+    @CurrentUser('userId') userId: number,
+
+  ) {
+    return this.paymentService.create(dto, userId);
+  }
+
+  @Get('callback/check')
+  async paymentCallback(@Query('reference') reference: string) {
+    return this.paymentService.verifyPayment(reference);
+  }
+}
