12db211d5398514791c020192c24a617281c4ee0
Uzochukwu Joseph <josephkingsleyy@gmail.com>
2026-02-20 11:43:08 +0100

    Updated CORS in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    // origin: true,
+    origin: process.env.FRONTEND_URL,
     methods: '*',
     allowedHeaders: '*',
     credentials: true,
